e536c83cc453848056e8f807da42205f5b8040ee
Karl Brightman <karl@karlbright.org>
2019-02-27 16:36:40 +0800

    Add basic syllable animation and structure without React

--- src/index.js
+++ src/index.js
@@ -1,55 +1,148 @@
-import paper from 'paper'
 import * as opentype from 'opentype.js'
-import { createStore } from 'redux'
+import * as easing from 'd3-ease'
+import paper from 'paper'
 
-const store = createStore((state = {}, action) => {
-  switch (action.type) {
-    case 'ERROR':
-      return { ...state, error: action.err }
-    case 'LOAD':
-      return { ...state, loaded: action.loaded }
-    case 'DRAW':
-      return { ...state, drawn: true }
-    default:
-      return state
+class Application {
+  constructor(canvas, font) {
+    this.canvas = canvas
+    this.font = font
+
+    paper.setup(canvas)
+    this.draw()
+
+    window.addEventListener('resize', this.update.bind(this))
   }
+
+  draw() {
+    this.background = new Background()
+    this.syllable = new Syllable(this.font, '집', () => {
+      this.background.in().then(this.syllable.in)
+    })
+  }
+
+  update() {
+    paper.view.viewSize = new paper.Size(window.innerWidth, window.innerHeight)
+    this.background.update()
+    this.syllable.update()
+  }
+}
+
+class Background {
+  constructor() {
+    this.draw()
+  }
+
+  draw() {
+    this.shape = new paper.Shape.Circle({
+      radius: 0,
+      center: paper.view.center,
+      fillColor: 'black'
+    })
+  }
+
+  update() {
+    this.shape.position = paper.view.center
+  }
+
+  in() {
+    return this.shape.tween(
+      { radius: Math.max(window.innerHeight, window.innerWidth) },
+      { duration: 500, easing: easing.easeCircleIn }
+    )
+  }
+}
+
+class Syllable {
+  constructor(font, string, cb) {
+    this.string = string
+    this.loading = true
+
+    opentype.load(font, (err, font) => {
+      if (err) return
+      this.font = font
+      this.draw()
+      this.loading = false
+      cb()
+    })
+
+    this.in = this.in.bind(this)
+  }
+
+  draw() {
+    this.circle = new paper.Shape.Circle({
+      radius: 0,
+      center: paper.view.center,
+      fillColor: 'white'
+    })
+
+    this.syllable = new Glyphs(this.font, this.string)
+    this.syllable.fillColor = 'black'
+    this.syllable.opacity = 0
+    this.syllable.fitBounds(new paper.Rectangle(paper.view.center, 200))
+    this.syllable.applyMatrix = false
+    this.syllable.position = paper.view.center
+  }
+
+  update() {
+    if (this.loading) return
+    this.circle.position = paper.view.center
+    this.syllable.position = paper.view.center
+  }
+
+  in() {
+    this.syllable.scale(1.5).opacity = 1
+    this.circle.tween({ radius: 200 }, { duration: 400, easing: easing.easeBackOut })
+    this.syllable.tween({ scaling: 1 }, { duration: 400, easing: easing.easeBackOut })
+  }
+}
+
+class Glyphs {
+  constructor(font, string) {
+    this.shape = new paper.CompoundPath()
+    font.stringToGlyphs(string).forEach(this.draw.bind(this))
+    return this.shape
+  }
+
+  draw(glyph) {
+    let path = new paper.Path()
+    glyph.path.commands.forEach(command => {
+      switch (command.type) {
+        case 'M':
+          path = new paper.Path()
+          this.shape.addChild(path)
+          path.moveTo(new paper.Point(command.x, command.y))
+          break
+
+        case 'L':
+          path.add(new paper.Point(command.x, command.y))
+          path.lineTo(new paper.Point(command.x, command.y))
+          break
+
+        case 'C':
+          path.cubicCurveTo(
+            new paper.Point(command.x1, command.y1),
+            new paper.Point(command.x2, command.y2),
+            new paper.Point(command.x, command.y)
+          )
+          break
+
+        case 'Q':
+          path.quadraticCurveTo(new paper.Point(command.x1, command.y1), new paper.Point(command.x, command.y))
+          break
+
+        case 'Z':
+          path.closed = true
+          break
+
+        default:
+          break
+      }
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('root')
+  const font = 'https://fonts.gstatic.com/s/blackhansans/v2/ea8Aad44WunzF9a-dL6toA8r8kqYK3M.woff'
+  new Application(canvas, font)
 })
-
-let shape = null
-
-function init() {
-  opentype.load('https://fonts.gstatic.com/s/blackhansans/v2/ea8Aad44WunzF9a-dL6toA8r8kqYK3M.woff', (err, font) => {
-    if (err) return store.dispatch({ type: 'ERROR', error: err })
-    store.dispatch({ type: 'LOAD', loaded: true })
-  })
-
-  const root = document.getElementById('root')
-  paper.setup(root)
-  paper.view.onResize = () => store.dispatch({ type: 'RESIZE' })
-}
-
-function step() {
-  const state = store.getState()
-  if (state.error) throw err
-  if (!state.loaded) return false
-  if (!state.drawn) return draw()
-  return update()
-}
-
-function draw() {
-  shape = new paper.Path.Circle({
-    radius: 50,
-    center: paper.view.center,
-    fillColor: 'red',
-    fullySelected: true
-  })
-
-  store.dispatch({ type: 'DRAW' })
-}
-
-function update() {
-  shape.position = paper.view.center
-}
-
-store.subscribe(step)
-document.addEventListener('DOMContentLoaded', init)
